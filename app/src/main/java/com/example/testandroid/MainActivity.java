85e245dce334d4b165d2cad44075b7da72671ff0
utkarshsharma <rickysharma1111@gmail.com>
2024-12-03 11:28:55 +0530

    Error fixed for loading unity as a library and launched it once

--- app/src/main/java/com/example/testandroid/MainActivity.java
+++ app/src/main/java/com/example/testandroid/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.testandroid;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.unity3d.player.UnityPlayerActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +24,14 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+       Button btnUnity = findViewById(R.id.launchUnity);
+
+       btnUnity.setOnClickListener(
+               v -> {
+                   Intent i = new Intent(MainActivity.this, UnityPlayerActivity.class);
+                   startActivity(i);
+               }
+       );
     }
 }
